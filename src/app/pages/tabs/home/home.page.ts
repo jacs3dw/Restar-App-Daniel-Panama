db2437b567742ae9b351054822ff94aea7cda194
BreynnerJacs <desarrollo33@jacs3d.com>
2025-11-26 11:22:28 -0500

    Nuevo numero

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -9,12 +9,8 @@ interface ShipmentStop {
 }
 
 interface Shipment {
-  trackingNumber: string;
   status: 'Pending' | 'Completed';
-  statusColor?: string;
-  origin: ShipmentStop;
-  destination: ShipmentStop;
-
+  id: string;
   title: string;
   url: string;
   description: string;
@@ -73,63 +69,29 @@ export class HomePage implements OnInit {
   ];
 
   loadShipments() {
+    //Realizar peticion backend
     this.allShipments = [
       {
-        trackingNumber: '#124 784 8754',
+        id: '1',
         status: 'Completed',
-        statusColor: '#',
-        origin: {
-          icon: 'ellipse-outline',
-          city: 'Amsterdam, Netherlands',
-          dateTime: '24 Jun, 16:32',
-        },
-        destination: {
-          icon: 'location-outline',
-          city: 'New York, USA',
-          dateTime: '30 Jun, 08:41',
-        },
         title: 'Package from Amsterdam to New York',
         url: 'https://www.google.com/',
         description: 'Your package is being processed and will be shipped soon.',
       },
       {
-        trackingNumber: '#874 455 2211',
+        id: '2',
         status: 'Pending',
-        statusColor: '#4CAF50',
-        origin: {
-          icon: 'ellipse-outline',
-          city: 'Berlin, Germany',
-          dateTime: '10 Jul, 12:20',
-        },
-        destination: {
-          icon: 'location-outline',
-          city: 'Paris, France',
-          dateTime: '12 Jul, 09:15',
-        },
         title: 'Order delivered to Paris',
         url: 'https://www.youtube.com/',
         description: 'This shipment has been successfully delivered to the recipient.',
       },
       {
-        trackingNumber: '#998 123 7777',
+        id: '3',
         status: 'Pending',
-        statusColor: '#2196F3',
-        origin: {
-          icon: 'ellipse-outline',
-          city: 'Madrid, Spain',
-          dateTime: '15 Jul, 18:45',
-        },
-        destination: {
-          icon: 'location-outline',
-          city: 'Lisbon, Portugal',
-          dateTime: '16 Jul, 14:10',
-        },
         title: 'Shipment en route to Lisbon',
         url: 'https://www.bing.com/',
         description: 'Your package is on the way and will arrive very soon.',
       },
-        
-
     ];
 
 
