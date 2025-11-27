78574297f2cb7fcd3d748a21ed36cf47c5cb80e3
daiver14102006-dp <daiver14102006@gmail.com>
2025-11-27 09:27:09 -0500

    Ordenes de history

--- Api/services/entities_manager/indexEntitiesManager.ts
+++ Api/services/entities_manager/indexEntitiesManager.ts
@@ -57,3 +57,19 @@ export const getperfills = async () => {
 
     return infoErrorApi(data, 'Error al obtener el usuario');
 };
+
+export const getAllorders = async () => {
+    const response: ResponseApp = structuredClone(responseApi.responseApp);
+    const path = `/order-management/by-user`;
+
+    const respApi: ResponseApi = await fetchApiAuthenticated.get(path);
+    const { status, data } = respApi;
+
+    if (status) {
+        response.data = data.data; // <- Todas las órdenes del usuario
+        return response;
+    }
+
+    return infoErrorApi(data, 'Error al obtener las órdenes');
+};
+
